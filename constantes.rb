56aa88b0e665c1964dfb8e031edda409dd3ae153
Santiagvs <victor.lacsilva0102@gmail.com>
2021-02-19 13:34:17 -0300

    feat(class): learning about constants
    	learning about constants and its functions

---
+++ constantes.rb
@@ -0,0 +1,13 @@
+# Constantes são usadas com letras maiúsculas
+# ex:
+
+class Teste
+    PI = 3.14
+    NOME_APP = "Sistema de CRM"
+    NOME_CLIENTE = "FULANO"
+end
+
+puts Teste::PI
+puts Teste::NOME_APP
+puts Teste::NOME_CLIENTE
+# a constante é para ficar imóvel, não é para ser alterada constantemente, diferente das variáveis
